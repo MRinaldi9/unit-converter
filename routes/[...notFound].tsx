9d0ca744d0a690b69605e443d296807133249bda
Marco Rinaldi <marco.rinaldi9@live.com>
2025-08-21 13:45:01 +0200

    Add NotFound component for handling 404 errors

---
+++ routes/[...notFound].tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div class='flex flex-col items-center justify-center gap-4 p-4'>
+      <h1>404 - Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <a href='/' class='btn btn-primary'>Go to Home</a>
+    </div>
+  );
+}
